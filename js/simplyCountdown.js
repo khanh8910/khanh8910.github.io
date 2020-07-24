bb0223027140aad30baa1a78c1de24747ae3d785
khanh8910 <baokhanh234@gmail.com>
2020-07-24 21:11:19 +0700

    Update simplyCountdown.js

--- js/simplyCountdown.js
+++ js/simplyCountdown.js
@@ -115,18 +115,18 @@
      */
     simplyCountdown = function (elt, args) {
         var parameters = extend({
-                year: 2015,
-                month: 6,
-                day: 28,
+                year: 0,
+                month: 0,
+                day: 0,
                 hours: 0,
                 minutes: 0,
                 seconds: 0,
                 words: {
-                    days: 'day',
-                    hours: 'hour',
-                    minutes: 'minute',
-                    seconds: 'second',
-                    pluralLetter: 's'
+                    days: 'ngày',
+                    hours: 'tiếng',
+                    minutes: 'phút',
+                    seconds: 'giây',
+                    pluralLetter: ''
                 },
                 plural: true,
                 inline: false,
